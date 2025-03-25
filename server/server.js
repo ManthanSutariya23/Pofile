21d471fb8bf5fda76c294c492b211a1694fe35f5
ManthanSutariya23 <manthan.j.sutariya@gmail.com>
2025-03-25 11:34:48 +0000

    add index file in server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname));
 
 app.post("/send-email", async (req, res) => {
     const { name, email, message, subject } = req.body;
